44aa43adf3084dcbde289dc927aef8e7ee747229
kikilikiki <132208953+kikilikiki@users.noreply.github.com>
2026-08-17 14:30:28 +0200

    H135 : version 0.3.2 du Launcher (force la mise a jour) + corrige le paquet Linux (.deb/.tar.gz) qui ne contenait pas les DLL du Client + allege l'installateur Windows (277 Mo -> 79 Mo)

--- Shared/GameInfo.cs
+++ Shared/GameInfo.cs
@@ -11,7 +11,7 @@ public static class GameInfo
     public const string Name = "Aetheria";
 
     /// <summary>Version courante du jeu (SemVer).</summary>
-    public const string Version = "0.3.1";
+    public const string Version = "0.3.2";
 
     /// <summary>Port TCP par défaut utilisé par le serveur de jeu.</summary>
     public const int DefaultGamePort = 7777;
